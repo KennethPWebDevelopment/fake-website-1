c5a05aa7052a3a2bb95439edc8c61b109c621740
Kenneth Paige <KEPAIGE@atl.com>
2022-04-18 09:01:49 -0400

    gh deploy

---
+++ build/precache-manifest.25f2f4327ae4260dcd8919364de0144b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8873c0873354b79086d102af6926b5df",
+    "url": "/fake-website-1/index.html"
+  },
+  {
+    "revision": "e872e70539eeadb939bd",
+    "url": "/fake-website-1/static/css/main.7f9587ff.chunk.css"
+  },
+  {
+    "revision": "9ae52d7ece48570a24e5",
+    "url": "/fake-website-1/static/js/2.7e273115.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/fake-website-1/static/js/2.7e273115.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e872e70539eeadb939bd",
+    "url": "/fake-website-1/static/js/main.b4cab12c.chunk.js"
+  },
+  {
+    "revision": "68f1a3b21656e9bab9e6",
+    "url": "/fake-website-1/static/js/runtime-main.99d7e6ac.js"
+  },
+  {
+    "revision": "9475b49e41a8dd7df9fbab4c5c0fbb71",
+    "url": "/fake-website-1/static/media/hero-image.9475b49e.png"
+  }
+]);
